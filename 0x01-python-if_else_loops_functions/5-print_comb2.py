29c9d7e3aa4c66838ed021484458e0a33950b19d
Egba Nnaemeka <emekason128@gmail.com>
2022-08-25 06:07:02 +0100

    done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("{:02d}, ".format(i), end='')
-    elif i > 10 and i < 99:
-        print("{}, ".format(i), end='')
-    else:
-        print("{}".format(i), end='')
+    print{"{:02d}".format(num), end="\n" if i == 99 else ", ")
