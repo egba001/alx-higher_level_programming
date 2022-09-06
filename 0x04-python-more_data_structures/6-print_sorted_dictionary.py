8b69695311b8264986abd6d34e11c4e2d0a50611
Egba Nnaemeka <emekason128@gmail.com>
2022-09-06 15:57:06 +0100

    Added

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    ordered = list(a.dictionary.keys())
+    ordered.sort()
+    for i in ordered:
+        print("{}: {}".format(i, a.dictionary.get(i))
+        
+        
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    list_of_keys = list(new_dict.keys())
+
+    for value in list_of_keys:
+        new_dict[values] *= 2
+
+    return (new_dict)
